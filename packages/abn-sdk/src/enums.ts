ea32e4b27c4b65a672052ec040d99808af52e567
diego <diego@bizcuit.nl>
2023-06-23 11:31:51 +0200

    feat(abn-sdk): add ReadSEPAPayment scope

--- packages/abn-sdk/src/enums.ts
+++ packages/abn-sdk/src/enums.ts
@@ -1,5 +1,6 @@
 export enum ABNScopes {
   PostSEPAPayment = 'psd2:payment:sepa:write',
+  ReadSEPAPayment = 'psd2:payment:sepa:read',
   PostXBorderPayment = 'psd2:payment:xborder:write',
   PostSEPAStandingOrderPayment = 'psd2:payment:recurrent:sepa:write'
 }
