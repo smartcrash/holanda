0933a267ef0098af94740f72fab49e0020907847
diego <diego@bizcuit.nl>
2023-06-23 11:48:51 +0200

    feat(abn-sdk): update ABNScopes

--- packages/abn-sdk/src/enums.ts
+++ packages/abn-sdk/src/enums.ts
@@ -1,13 +1,28 @@
 export enum ABNScopes {
-  /** Post (structured) SEPA payment */
+  /** Execute or cancel SEPA payment */
   PostSEPAPayment = 'psd2:payment:sepa:write',
 
   /** Check SEPA payment status */
   ReadSEPAPayment = 'psd2:payment:sepa:read',
 
+  /** Execute SEPA standing order payment */
+  PostSEPARecurrentPayment = 'psd2:payment:recurrent:sepa:write',
+
+  /** Cancel SEPA standing order payment	 */
+  DeleteSEPARecurrentPayment = 'psd2:payment:recurrent:sepa:delete',
+
   /** Post XBorder payment */
   PostXBorderPayment = 'psd2:payment:xborder:write',
 
-  /** Post SEPA standing order payment */
-  PostSEPAStandingOrderPayment = 'psd2:payment:recurrent:sepa:write'
+  /** Read the balance of an account */
+  ReadAccountBalance = 'psd2:account:balance:read',
+
+  /** Read the transactions/mutations on an account	*/
+  ReadAccountTransaction = 'psd2:account:transaction:read',
+
+  /** Read the details of an account, such as address and currency */
+  ReadAccountDetails = 'psd2:account:details:read',
+
+  /** Check availability of funds on an account */
+  ReadAccountFunds = 'psd2:account:funds:read',
 }
