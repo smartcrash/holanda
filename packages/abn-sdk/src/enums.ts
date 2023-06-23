12bfb343aa1dbc91ab37ae1f5bc083a0465b208b
diego <diego@bizcuit.nl>
2023-06-23 11:40:46 +0200

    docs(abn-sdk): add comments to ABNScopes enum

--- packages/abn-sdk/src/enums.ts
+++ packages/abn-sdk/src/enums.ts
@@ -1,6 +1,13 @@
 export enum ABNScopes {
+  /** Post (structured) SEPA payment */
   PostSEPAPayment = 'psd2:payment:sepa:write',
+
+  /** Check SEPA payment status */
   ReadSEPAPayment = 'psd2:payment:sepa:read',
+
+  /** Post XBorder payment */
   PostXBorderPayment = 'psd2:payment:xborder:write',
+
+  /** Post SEPA standing order payment */
   PostSEPAStandingOrderPayment = 'psd2:payment:recurrent:sepa:write'
 }
